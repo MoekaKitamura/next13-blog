f5a7c526a173469e5ed99c8f831e2a481e5c1aa5
moekakitamura <kitamura@livesearch.co.jp>
2023-11-12 21:03:55 +0900

    post blog function

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { createArticle } from "@/blogAPI";
 
 const page = () => {
+  const router = useRouter();
+  const [id, setId] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await createArticle(id, title, content);
+
+    router.push("/");
+    router.refresh();
+  };
+
   return (
     <div className="min-h-screen py-8 px-4 md:px-12">
       <h2 className="text-2xl font-bold mb-4">ブログ新規作成</h2>
-      <form className="bg-slate-200 p-6 rounded shadow-lg">
+      <form
+        className="bg-slate-200 p-6 rounded shadow-lg"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4">
           <label className="text-gray-700 text-sm font-bold mb-2">URL</label>
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -19,11 +41,17 @@ const page = () => {
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="mb-4">
           <label className="text-gray-700 text-sm font-bold mb-2">本文</label>
-          <textarea className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" />
+          <textarea
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
         <button
           type="submit"
